f18c0d4741fea0ee34082c20da0ef86ec3b940a5
Philipp Craine <125037167+philippzhuravlev@users.noreply.github.com>
2025-03-28 11:15:06 +0100

    Reapply "Type, Evaluation, Serializer integration"
    This reverts commit 6b9b4f3070d0d050b08439762afcf30e50262429.

--- src/main/java/dk/dtu/compute/course02324/mini_java/semantics/ProgramSerializerVisitor.java
+++ src/main/java/dk/dtu/compute/course02324/mini_java/semantics/ProgramSerializerVisitor.java
@@ -86,6 +86,13 @@ public class ProgramSerializerVisitor extends ProgramVisitor  {
         expessionRepresentations.put(operatorExpression, result);
     }
 
+    @Override
+    public void visit(PrintStatement printStatement) {
+        result = "println(\"" + printStatement.message + "\", " + 
+                expessionRepresentations.get(printStatement.expression) + ")";
+        statementRepresentations.put(printStatement, result);
+    }
+
     private String operandToString(Operator operator, Expression expression, int number) {
         String result = expessionRepresentations.get(expression);
         if (expression instanceof OperatorExpression) {
