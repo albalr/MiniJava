60d7d85ba152aeb452118947128f4e42f3950e97
Philipp Craine <125037167+philippzhuravlev@users.noreply.github.com>
2025-03-28 10:44:41 +0100

    add to Program Type Visitor

--- src/main/java/dk/dtu/compute/course02324/mini_java/semantics/ProgramTypeVisitor.java
+++ src/main/java/dk/dtu/compute/course02324/mini_java/semantics/ProgramTypeVisitor.java
@@ -18,7 +18,9 @@ public class ProgramTypeVisitor extends ProgramVisitor {
     final private Map<Operator,List<Type>> operatorTypes = Map.ofEntries(
             entry(PLUS2, List.of(INT, FLOAT)),
             entry(MINUS2, List.of(INT, FLOAT)),
-            entry(MULT, List.of(INT, FLOAT)));
+            entry(MULT, List.of(INT, FLOAT)),
+            entry(DIV, List.of(INT, FLOAT)),
+            entry(MOD, List.of(INT)));
 
     final public Map<Expression, Type> typeMapping = new HashMap<>();
 
